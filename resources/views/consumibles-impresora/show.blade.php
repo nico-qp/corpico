995986fa602a51ecacd5cddf201c89ec63174730
sistemas <sistemas@COOPPICO.NET>
2023-09-21 09:23:24 -0300

    Show de consumible impresora armado

--- resources/views/consumibles-impresora/show.blade.php
+++ resources/views/consumibles-impresora/show.blade.php
@@ -21,14 +21,36 @@
                     <div class="card-body">
                         
                         <div class="form-group">
+                            <strong>Consumible:</strong>
+                            <br>
                             <strong>Codigo del Consumible:</strong>
                             {{ $consumiblesImpresora->consumible->codigo }}
+                            <br>
+                            <strong>Tipo del Consumible:</strong>
+                            {{ $consumiblesImpresora->consumible->TipoConsumible->nombre }}
+                            <br>
+                            <strong>Uso del Consumible:</strong>
+                            {{ $consumiblesImpresora->consumible->Uso->mensual }}
+                            <br>
+                            <strong>Descripcion del Consumible:</strong>
+                            {{ $consumiblesImpresora->consumible->descripcion }}
+                            <br>
                         </div>
                         <div class="form-group">
+                            <strong>Impresora:</strong>
+                            <br>
                             <strong>IP de la impresora:</strong>
                             {{ $consumiblesImpresora->impresora->ip }}
+                            <br>
+                            <strong>Sector de la impresora:</strong>
+                            {{ $consumiblesImpresora->impresora->Sectore->nombre }}
+                            <br>
+                            <strong>Criticidad de la impresora:</strong>
+                            {{ $consumiblesImpresora->impresora->Criticidade->descripcion }}
+                            <br>
+                            <strong>Autonomia de la resma de la impresora:</strong>
+                            {{ $consumiblesImpresora->impresora->autonomia_resma }}
                         </div>
-
                     </div>
                 </div>
             </div>

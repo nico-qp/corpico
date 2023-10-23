aacc9fc5f5eb5bf3b570064c07ad2edb32167837
Mateo-Bazan <126024709+Mateo-Bazan@users.noreply.github.com>
2023-10-23 12:12:02 -0300

    Update create.blade.php

--- resources/views/criticidade/create.blade.php
+++ resources/views/criticidade/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Cargar') }} una Criticidad</span>
+                        <span class="card-title">{{ __('Cargar') }} Criticidad</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('criticidades.store') }}"  role="form" enctype="multipart/form-data">

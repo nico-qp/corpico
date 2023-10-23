d259b3d98c7330788c4c690482839833ada3629c
Mateo-Bazan <126024709+Mateo-Bazan@users.noreply.github.com>
2023-10-23 12:11:39 -0300

    Update create.blade.php

--- resources/views/ubicacione/create.blade.php
+++ resources/views/ubicacione/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Cargar') }} una ubicacion</span>
+                        <span class="card-title">{{ __('Cargar') }} Ubicación</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('ubicaciones.store') }}"  role="form" enctype="multipart/form-data">

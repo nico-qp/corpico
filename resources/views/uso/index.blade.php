bc2b2de3f7e9df05ecefd71c4cae60ff6398d524
Mateo-Bazan <126024709+Mateo-Bazan@users.noreply.github.com>
2023-10-23 11:53:42 -0300

    Update index.blade.php

--- resources/views/uso/index.blade.php
+++ resources/views/uso/index.blade.php
@@ -13,7 +13,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Uso') }}
+                                {{ __('Usos') }}
                             </span>
 
                              <div class="float-right">

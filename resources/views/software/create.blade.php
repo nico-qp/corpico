362f5d4ac54dc1acf25c72f40b39ec1390ff6df2
Mateo-Bazan <126024709+Mateo-Bazan@users.noreply.github.com>
2023-10-23 12:10:24 -0300

    Update create.blade.php

--- resources/views/software/create.blade.php
+++ resources/views/software/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Cargar') }} un Software</span>
+                        <span class="card-title">{{ __('Cargar') }} Software</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('softwares.store') }}"  role="form" enctype="multipart/form-data">

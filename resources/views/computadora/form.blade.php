63acd09ded821c2a4005c9e84c6dfa9f225a638a
Nico Lovera <118082210+nico-qp@users.noreply.github.com>
2023-10-30 10:55:56 -0300

    Update form.blade.php

--- resources/views/computadora/form.blade.php
+++ resources/views/computadora/form.blade.php
@@ -32,13 +32,13 @@
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('ip_172') }}
-            {{ Form::text('ip_172', $computadora->ip_172, ['class' => 'form-control' . ($errors->has('ip_172') ? ' is-invalid' : ''), 'placeholder' => 'Ip 172']) }}
+            {{ Form::label('IP_172') }}
+            {{ Form::text('ip_172', $computadora->ip_172, ['class' => 'form-control' . ($errors->has('ip_172') ? ' is-invalid' : ''), 'placeholder' => 'IP 172']) }}
             {!! $errors->first('ip_172', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('ip_10') }}
-            {{ Form::text('ip_10', $computadora->ip_10, ['class' => 'form-control' . ($errors->has('ip_10') ? ' is-invalid' : ''), 'placeholder' => 'Ip 10']) }}
+            {{ Form::label('IP_10') }}
+            {{ Form::text('ip_10', $computadora->ip_10, ['class' => 'form-control' . ($errors->has('ip_10') ? ' is-invalid' : ''), 'placeholder' => 'IP 10']) }}
             {!! $errors->first('ip_10', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
